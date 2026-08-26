86d8660914a3bc4e0cb846a020620b2816c87345
Athur Vitor <arthurvitor8500@mail.com>
2026-08-26 01:23:12 -0300

    commit

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,13 +1,19 @@
+import React from 'react';
 import Styles from './styles.module.css';
 
-type LinkProps = {
-    children: React.ReactNode,
-    attribute: string,
-};
+interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  children: React.ReactNode,
+  onClick?: (e) => void,
+}
 
-export function Link({ children }: LinkProps) {
+export const Link: React.FC<LinkProps> = ({
+  children,
+  href,
+  onClick,
+  ...rest
+}) => {
   return (
-      <a className={Styles.link} href="#">
+      <a className={Styles.link} href={href} onClick={onClick} {...rest}>
         {children}
       </a>
   );
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,8 +1,29 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon, TimerIcon } from 'lucide-react';
 import { Link } from '../Link';
 import Styles from './styles.module.css';
+import React, { useEffect, useState } from 'react';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
 
 export function Menu() {
+
+  type prevTheme = 'dark' | 'ligth';
+
+  const [ theme, setTheme ] = useState<prevTheme>('dark');
+
+  function hendleThemeChange(
+    evt: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    evt.preventDefault();
+    setTheme(prev => {
+      const nextTheme = prev === 'dark' ? 'ligth' : 'dark';
+      return nextTheme;
+    })
+  }
+
+  useLocalStorage('theme', useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [ theme ]));
+
   return (
     <nav className={Styles.menu}>
         <Link>
@@ -17,7 +38,10 @@ export function Menu() {
           <SettingsIcon />  
         </Link>
 
-        <Link>
+        <Link
+          href='#'
+          onClick={hendleThemeChange}
+        >
           <SunIcon />  
         </Link>
     </nav>
